d8195c440fa351ce9a598ef4ddcb4fff9c6fda9b
Ramon Rasheed <ramonrash2@gmail.com>
2024-11-20 11:06:20 +0100

    team card responsive

--- src/features/roles/components/team-card.tsx
+++ src/features/roles/components/team-card.tsx
@@ -57,7 +57,7 @@ export default function TeamCard() {
 
   return (
     <Card className="shadow-none border-none">
-      <CardContent className="flex items-center justify-between py-6">
+      <CardContent className="flex flex-col md:flex-row gap-6 md:items-center justify-between py-6">
         <div className="flex flex-col gap-0.5">
           <h2 className="text-xl font-bold text-text">Team Members</h2>
           <p className="font-medium text-gray-500">
